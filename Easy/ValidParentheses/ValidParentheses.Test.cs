cca9e662897cbd82ee1d8a55244922ecdb48d273
Andree Galvez Conejeros <18559215+andreegalv@users.noreply.github.com>
2025-02-26 23:49:40 -0300

    Added Easy.ValidParentheses LeetCode

---
+++ Easy/ValidParentheses/ValidParentheses.Test.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Easy.ValidParentheses
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        [DataRow("()", true)]
+        [DataRow("()[]{}", true)]
+        [DataRow("(]", false)]
+        [DataRow("([])", true)]
+        [DataRow("([)]", false)]
+        [DataRow("([])]", false)]
+        [DataRow("]", false)]
+        [DataRow("([])]{}", false)]
+        [DataRow("}([)", false)]
+        [DataRow("([)", false)]
+        public void Execute(string text, bool expected)
+        {
+            Assert.AreEqual(expected, ValidParentheses.Execute(text));
+        }
+    }
+}
---
+++ Easy/ValidParentheses/ValidParentheses.cs
@@ -0,0 +1,57 @@
+﻿namespace LeetCode.Easy.ValidParentheses
+{
+    public static class ValidParentheses
+    {
+        private static Dictionary<char, char> _openedParentheses = new()
+        {
+            { '(', ')' },
+            { '{', '}' },
+            { '[', ']' }
+        };
+
+        private static Dictionary<char, char> _closedParentheses = new()
+        {
+            { ')', '(' },
+            { '}', '{' },
+            { ']', '[' }
+        };
+
+        public static bool Execute(string s)
+        {
+            List<char> _parenthesesOrderedStep = [];
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char parenthes = s[i];
+
+                if (i == 0 && !_openedParentheses.ContainsKey(parenthes))
+                {
+                    return false;
+                }
+
+                if (_openedParentheses.ContainsKey(parenthes))
+                {
+                    _parenthesesOrderedStep.Add(parenthes);
+                }
+                else
+                {
+                    if (_parenthesesOrderedStep.Count <= 0)
+                    {
+                        return false;
+                    }
+
+                    if (_parenthesesOrderedStep.Last() == _closedParentheses[parenthes])
+                    {
+                        _parenthesesOrderedStep.RemoveAt(_parenthesesOrderedStep.Count - 1);
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return _parenthesesOrderedStep.Count == 0;
+        }
+    }
+}
